19668cfbb2d71f33096820c00dadf046d4cd48c9
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-05-26 10:17:01 -0400

    new folder

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,19 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
